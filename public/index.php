4353def242fd7485b7faedac97752f45006f7529
David Pauli <david.pauli@check24.de>
2021-05-15 23:24:35 +0200

    Initial try

--- public/index.php
+++ public/index.php
@@ -1,1 +1,21 @@
 <?php
+
+use CzProject\GitPhp\Git;
+use RedBum\Configuration\RepositoryConfiguration;
+
+require_once ('../vendor/autoload.php');
+
+// PHP settings
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 'Off');
+ini_set('log_errors', 'On');
+ini_set('error_log', sprintf('%s/%s-error.log', '../log', date('Y-m-d')));
+
+$config = json_decode(file_get_contents("../config.json"), true);
+
+$repositoryConfig = new RepositoryConfiguration($config['repository']);
+
+$git = new Git();
+#$git->cloneRepository($repositoryConfig->source(), '../' . $repositoryConfig->directory());
+$repo = $git->open('../' . $repositoryConfig->directory());
+$repo->fetch()->pull('origin');
--- src/Configuration/RepositoryConfiguration.php
+++ src/Configuration/RepositoryConfiguration.php
@@ -1,7 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
+namespace RedBum\Configuration;
 
 class RepositoryConfiguration
 {
+    /** @var string */
+    private $directory;
 
+    /** @var string */
+    private $source;
+
+    public function __construct(array $configuration)
+    {
+        $this->directory = $configuration['directory'];
+        $this->source = $configuration['source'];
+    }
+
+    public function directory(): string
+    {
+        return $this->directory;
+    }
+
+    public function source(): string
+    {
+        return $this->source;
+    }
 }
